ce3185649b5fd0137064a13f66ced89dafd65317
ashish-pro <ashishbit017@outlook.com>
2024-01-18 19:06:08 +0530

    created routes and pages

--- blog-app-client/src/App.jsx
+++ blog-app-client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">  App</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/sign-in' element= {<Signin/>} />
+          <Route path='/sign-up' element={<SignUp />}/>
+        </Routes>
+
+    </BrowserRouter>
   )
 }
 

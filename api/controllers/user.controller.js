88f4a1b04527b51ce958b7e0ab9df038e26b7c66
ashish-pro <ashishbit017@outlook.com>
2024-02-22 18:14:19 +0530

    bugfixes: bug fixes on signup

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,7 @@
-export const test = (req, res) => {
-    res.json({message : 'API is working'});
-};
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import User from '../models/user.models.js';
 
+export const test = (req, res) => {
+  res.json({ message: 'API is working!' });
+};

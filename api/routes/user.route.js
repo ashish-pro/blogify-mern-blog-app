dd07b501ed7c779434954bf0a2818bf141d74bda
ashish-pro <ashishbit017@outlook.com>
2024-02-23 21:21:03 +0530

    add admin routes / functionalities

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,5 +8,5 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-
+ 
 export default router;
